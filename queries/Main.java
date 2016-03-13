be91beddd5a7c0ae08fd0a3e9dacd1ba5030e0c0
Hamyli <hamyli1993@gmail.com>
2016-03-13 13:27:53 -0400

    z

--- queries/Main.java
+++ queries/Main.java
@@ -7,8 +7,8 @@ import java.net.URL;
 
 public class Main {
     public static void main (String [] args) throws Exception {
-        //query1();
-        //query2();
+        query1();
+        query2();
         query3();
     }
 
@@ -34,7 +34,7 @@ public class Main {
         Client client = Client.create();
 
         WebResource webResource = client
-                .resource("http://localhost:8080/FantasySports/query/getCustomQueryNew");
+                .resource("http://localhost:8080/FantasySports/query/getCustomQuery");
 
         String input = "{\"query\":\"Select * from NBAPlayerStats as a, NBAPlayer as b where playerName = 'Stephen Curry' and `3P` >= 10 and a.playerID = b.playerID\",\"token\":\"3a2993df-e8a2-11e5-9f31-0228a770e05b\"}";
 
@@ -52,6 +52,21 @@ public class Main {
                 .resource("http://localhost:8080/FantasySports/query/getNBAAdvancedStats");
 
         String input = "{\"playerName\":\"Stephen Curry\", \"PTS\":\"30\", \"AST\":\"5\",\"token\":\"3a2993df-e8a2-11e5-9f31-0228a770e05b\"}";
+
+        String response = webResource.type("application/json")
+                .post(String.class, input);
+        JSONArray ja = new JSONArray(response);
+        String csv = CDL.toString(ja);
+        System.out.println(csv);
+    }
+
+    private static void query4() throws Exception {
+        Client client = Client.create();
+
+        WebResource webResource = client
+                .resource("http://localhost:8080/FantasySports/query/getNHLAdvancedStats");
+
+        String input = "{\"playerName\":\"Sidney Crosby\", \"G\":\"2\",\"token\":\"3a2993df-e8a2-11e5-9f31-0228a770e05b\"}";
 
         String response = webResource.type("application/json")
                 .post(String.class, input);
